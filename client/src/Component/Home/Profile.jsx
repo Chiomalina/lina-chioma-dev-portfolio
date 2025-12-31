ccc21612fe4fd78e058fb1410af46de7d0d44db7
Chiomalina <chiomalinaanaso@gmail.com>
2025-12-31 06:04:03 +0100

    create universal font

--- client/src/Component/Home/Profile.jsx
+++ client/src/Component/Home/Profile.jsx
@@ -11,62 +11,64 @@ function Profile() {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="https://www.facebook.com/profile.php?id=61556737950018">
-              <IoLogoFacebook color="black" />
-            </a>
-            <a href="#">
-              <AiFillGooglePlusSquare color="black" />
-            </a>
-            <a href="https://www.instagram.com/chiomaanaso.codes/">
-              <FaInstagram color="black" />
-            </a>
-            <a href="https://www.youtube.com/@ChiomaAnaso-b5m4y">
-              <TfiYoutube color="black" />
-            </a>
-            <a href="#">
-              <FaTwitter color="black" />
-            </a>
-
-            <div className="profile-details-name">
-              <span>
-                {" "}
-                Hello, I'm <span className="highlighted-text">Chioma</span>
-              </span>
+            <div className="colz-icon">
+              <a href="https://www.facebook.com/profile.php?id=61556737950018">
+                <IoLogoFacebook color="black" />
+              </a>
+              <a href="#">
+                <AiFillGooglePlusSquare color="black" />
+              </a>
+              <a href="https://www.instagram.com/chiomaanaso.codes/">
+                <FaInstagram color="black" />
+              </a>
+              <a href="https://www.youtube.com/@ChiomaAnaso-b5m4y">
+                <TfiYoutube color="black" />
+              </a>
+              <a href="#">
+                <FaTwitter color="black" />
+              </a>
             </div>
-
-            {/* Typewriter Effect*/}
-            <div className="profile-details-role">
-              <span className="primary-text">
-                {" "}
-                <h1>
-                  {" "}
-                  <span>
-                    <Typewriter
-                      words={[
-                        "💻 a Full-Stack Developer",
-                        "🚀 building scalable web apps",
-                        "🎨 creating clean APIs & UIs",
-                        "🧠 turning ideas into products",
-                      ]}
-                      loop={0}
-                      cursor
-                      cursorStyle="|"
-                      typeSpeed={80}
-                      deleteSpeed={50}
-                      delaySpeed={1200}
-                    />
-                  </span>
-                </h1>
-              </span>
-            </div>
-
-            {/* Description*/}
-            <h1>
-              <span className="profile-role-tagline">
-                Building applications with fron and backend operations
-              </span>
-            </h1>
           </div>
+
+          <div className="profile-details-name">
+            <span>
+              {" "}
+              Hello, I'm <span className="highlighted-text">Chioma</span>
+            </span>
+          </div>
+
+          {/* Typewriter Effect*/}
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <span>
+                  <Typewriter
+                    words={[
+                      "💻 a Full-Stack Developer",
+                      "🚀 building scalable web apps",
+                      "🎨 creating clean APIs & UIs",
+                      "🧠 turning ideas into products",
+                    ]}
+                    loop={0}
+                    cursor
+                    cursorStyle="|"
+                    typeSpeed={80}
+                    deleteSpeed={50}
+                    delaySpeed={1200}
+                  />
+                </span>
+              </h1>
+            </span>
+          </div>
+
+          {/* Description*/}
+          <h1>
+            <span className="profile-role-tagline">
+              Building applications with fron and backend operations
+            </span>
+          </h1>
 
           {/* Profile Options*/}
           <div className="profile-options">
@@ -78,6 +80,11 @@ function Profile() {
               <button className="btn highlighted-btn">Get Resume</button>
             </a>
           </div>
+        </div>
+
+        {/* Picture Component*/}
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
         </div>
       </div>
     </div>
